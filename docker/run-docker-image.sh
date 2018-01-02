c5882f0694137d4bc9f03ea1b449ed2eb7f9dcc1
Jason Nerothin <jasonnerothin@gmail.com>
2018-01-02 11:02:33 -0600

    dockerizing

---
+++ docker/run-docker-image.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash -x
+
+# execute this script from the directory containing this script, please
+
+readonly scriptarg="$1"
+readonly currentdir=$(pwd)
+
+readonly bindport=2718
+readonly bindiface=0.0.0.0
+readonly siteidentifier='robotrobot.io'
+
+# work dir for new builds from github
+readonly workdir=$(mktemp -d /tmp/robotrobot.io-XXXXXXX)
+readonly robotrobotdir=${workdir}/${siteidentifier}
+readonly themedir=${robotrobotdir}/themes
+
+# temp dirs to skip git
+readonly cachedsitedir=/tmp/${siteidentifier}
+if [ ! -d ${cachedsitedir} ]; then mkdir ${cachedsitedir} ; fi
+
+echo "INFO: Created work directory: ${workdir}"
+cd ${workdir}
+
+if [ "${scriptarg}" == "-cache" ];
+then
+   echo "INFO: Syncing last github pull from cache at: ${cachedsitedir}"
+   rsync -avh ${cachedsitedir} ${siteidentifier}
+else
+   echo "INFO: Pulling down from github and caching to: ${cachedsitedir}"
+   cd ${workdir} ;
+   git clone https://github.com/jasonnerothin/${siteidentifier}.git ;
+   cd ${themedir} ;
+   git clone https://github.com/MarcusVirg/forty ;
+   cd ${currentdir} ;
+   rsync -avh ${robotrobotdir} ${cachedsitedir} ;
+fi
+
+echo "Running ${siteidentifier} from ${robotrobotdir} at ${bindiface}:${bindport}..."
+docker run --rm -it -v ${robotrobotdir}:/src -p ${bindport}:1313 -u hugo jguyomard/hugo-builder hugo server -w --bind=${bindiface}
